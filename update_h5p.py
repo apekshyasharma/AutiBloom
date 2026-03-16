f0f562d50a36ee43cc832b64c87659f95e6dcc60
apekshyasharma <apekshyasharma2308@gmail.com>
2026-03-16 14:30:00 +0545

    Added Sensory Adventure and Social Story H5P game assets

--- update_h5p.py
+++ update_h5p.py
@@ -8,8 +8,7 @@ from games.models import TherapyGame
 
 mock_urls = {
     'emotion-match': 'https://h5p.org/h5p/embed/1386341',
-    'routine-builder': 'https://h5p.org/h5p/embed/1149729',
-    'calm-choices': 'https://h5p.org/h5p/embed/713601'
+    'routine-builder': 'https://h5p.org/h5p/embed/1149729'
 }
 
 for slug, url in mock_urls.items():
